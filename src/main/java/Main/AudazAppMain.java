179a468b12512acb4aaa17b1c41502554b2153b5
Danny Areiza <dannysteven89@gmail.com>
2021-02-27 20:53:21 -0500

    mi primer commit

---
+++ src/main/java/Main/AudazAppMain.java
@@ -0,0 +1,21 @@
+package Main;
+
+import Conexion.Conexion;
+
+import java.sql.Connection;
+
+public class AudazAppMain {
+    public static void main(String[] args) {
+
+        Conexion conexion = new Conexion();
+
+        try {
+            Connection cnx = conexion.get_connection();
+        } catch (Exception e) {
+            System.out.println(e);
+
+        }
+
+    }
+
+}
